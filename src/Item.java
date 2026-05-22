551fa516e6873c992eead8a341b4b587985f1403
Eter <eter.nyx@proton.me>
2026-05-22 08:48:15 -0300

    Added Polymorphism

--- src/Item.java
+++ src/Item.java
@@ -1,13 +1,22 @@
-public class Item {
-  String prato;
-  double valor;
+import java.util.Locale;
 
-  public Item (String p, double v) {
-    prato = p;
-    valor = v;
+public abstract class Item {
+  protected String nome;
+  protected double valor;
+
+  public Item(String nome, double valor) {
+    this.nome = nome;
+    this.valor = valor;
   }
 
-  public void lista() {
-    System.out.println ( "| " + prato + ": R$" + valor );
+  public String getNome() { return nome; }
+  public double getValor() { return valor; }
+
+  public abstract String getTipo();
+  public String precoFormat() {
+    return String.format(Locale.forLanguageTag("pt-BR"), "R$%.2f", valor);
+  }
+  public String descricao() {
+      return getTipo() + " - " + nome + ": " + precoFormat();
   }
 }
